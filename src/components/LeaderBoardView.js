2bbcd53f2a7bee224e35e0b43e312307525554d3
Heidi Duchting <h.duechting@gmx.de>
2020-04-16 16:37:12 +0100

    feat: Added Leaderboard content

--- src/components/LeaderBoardView.js
+++ src/components/LeaderBoardView.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
+import User from './User';
+import { connect } from 'react-redux';
 
 class LeaderBoardView extends Component {
 
   render() {
+    const { users } = this.props;
+    let usersArr =  Object.values(users).sort(
+      (a,b) => (
+        (Object.keys(b.answers).length + b.questions.length) - (Object.keys(a.answers).length + a.questions.length)
+      ));
     return (
-      <div>
-        LeaderBoardView
+      <div className="row">
+        <div className="row"></div>
+        <div className="row">
+          <div className="col s12">
+            <div className="row">
+              { usersArr.map((u) => (
+                <User key={u.id} user={u}/>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
+function mapStateToProps({ authedUser, questions, users }) {
+  return {
+    authedUser,
+    questions,
+    users,
+  };
+}
 
-export default LeaderBoardView;
+export default connect(mapStateToProps)(LeaderBoardView);
---
+++ src/components/User.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { handleAnswerQuestion } from '../actions/shared';
+
+class User extends Component {
+
+
+  render () {
+    const {user} = this.props;
+
+    return (
+
+      <div className="card">
+        <div className="row card-content">
+          <div className="row s12 teal lighten-4">
+            <div className="col"><h6><strong>{user.name}</strong></h6></div>
+          </div>
+          <div className="row s12">
+            <div className="col card-image s4"><img src={user.avatarURL} alt={user.name}/></div>
+            <div className="col s6">
+              <div className="row">
+                Answered Questions: {Object.keys(user.answers).length}
+              </div>
+              <div className="row">
+                Created Questions: {user.questions.length}
+              </div>
+            </div>
+            <div className="col s2 center-align">
+              <div className="row">
+                <h6>SCORE</h6>
+              </div>
+              <div className="row">
+                <div className='score teal-text text-darken-2'>{user.questions.length  + Object.keys(user.answers).length}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    );
+  }
+}
+
+function mapStateToProps ({authedUser, questions, users}) {
+  return {
+    authedUser,
+    questions,
+    users
+  };
+}
+
+export default withRouter(connect(mapStateToProps)(User));
