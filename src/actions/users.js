b8edab29737338fa296c71353be1f3b7d36ce49c
Heidi Duchting <h.duechting@gmx.de>
2020-04-16 18:11:09 +0100

    feat: added functionality to add a question

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,5 +1,6 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS';
 export const ADD_USER_QUESTION_ANSWER = 'ADD_USER_QUESTION_ANSWER';
+export const ADD_QUESTION_TO_USER = 'ADD_QUESTION_TO_USER';
 
 export function receiveUsers (users) {
   return {
@@ -15,5 +16,12 @@ export function addUserQuestionAnswer(authedUser, questionId, selectedAnswer) {
     authedUser,
     questionId,
     selectedAnswer
+  }
+}
+
+export function addQuestionToUser(question) {
+  return {
+    type: ADD_QUESTION_TO_USER,
+    question
   }
 }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import { RECEIVE_USERS, ADD_USER_QUESTION_ANSWER } from '../actions/users';
+import { RECEIVE_USERS, ADD_USER_QUESTION_ANSWER, ADD_QUESTION_TO_USER } from '../actions/users';
 
 export default function users (state = {}, action) {
   switch (action.type) {
@@ -16,6 +16,15 @@ export default function users (state = {}, action) {
             ...state[action.authedUser].answers,
             [action.questionId]: action.selectedAnswer
           }
+        }
+      };
+    case ADD_QUESTION_TO_USER:
+
+      return {
+        ...state,
+        [action.question.author]: {
+          ...state[action.question.author],
+          questions: [action.question.id].concat(...state[action.question.author].questions)
         }
       };
     default:
